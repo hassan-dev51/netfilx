87a942cc7585ea813883d3e124e7d2281e497c36
hassan-dev51 <hassanmunir363@gmail.com>
2023-08-07 16:04:42 +0500

    redirection issue solved

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,28 @@
 "use client";
 
 import { signOut } from "next-auth/react";
-
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
+import { useEffect } from "react";
 export default function Home() {
+  const { data: session, status } = useSession();
+  const router = useRouter();
+  useEffect(() => {
+    if (status === "unauthenticated" && !session) {
+      router.push("/auth");
+    }
+  }, [status, session, router]);
+
   return (
     <div>
       <h1>Netfilx clone</h1>
-
-      <button onClick={() => signOut()}>Sign Out</button>
+      {!session && (
+        <div>
+          <Link href="/auth">Please sign in to continue</Link>
+        </div>
+      )}{" "}
+      <button onClick={() => signOut()}> {session && <h1>Sign Out</h1>}</button>
     </div>
   );
 }
