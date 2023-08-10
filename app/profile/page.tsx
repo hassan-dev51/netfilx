e7927d9457b43970847bb15d3dfb38922ae55681
hassan-dev51 <hassanmunir363@gmail.com>
2023-08-10 20:51:39 +0500

    profile page update

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,5 +1,6 @@
 import { redirect } from "next/navigation";
-
+import Link from "next/link";
+import Image from "next/image";
 import { getServerSession } from "next-auth/next";
 
 import { option } from "../api/auth/[...nextauth]/option";
@@ -10,7 +11,25 @@ const Profile = async () => {
     redirect("/auth");
   }
 
-  return <div>Profile</div>;
+  return (
+    <div className="flex items-center h-full justify-center">
+      <div className="flex flex-col">
+        <h1 className="text-3xl md:text-6xl text-white text-center">
+          Who&#39;s watching? {session.user?.name}
+        </h1>
+        <div className="flex items-center justify-center gap-8 mt-10">
+          <Link href="/">
+            <Image
+              src={session?.user?.image || "/images/default-blue.png"}
+              alt={session?.user?.name || ""}
+              width={300}
+              height={300}
+            />
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
